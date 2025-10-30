dee24c4571cdb69a50c6570249407e746f75e0fd
tempkjs <kunaljorofficial@gmail.com>
2025-10-30 09:04:56 +0530

    added thr api key header in testconfig

--- src/config/testConfig.ts
+++ src/config/testConfig.ts
@@ -3,8 +3,11 @@
 export const config = {
   baseUrlUI: process.env.BASE_URL_UI || "https://demoqa.com",
   baseUrlAPI: process.env.BASE_URL_API || "https://reqres.in/api",
-  USER_EMAIL:"kjplhyb",
-USER_PASSWORD:"Admin@123",
+  USER_EMAIL: "kjplhyb",
+  USER_PASSWORD: "Admin@123",
   timeout: 30000,
-  headers: { "Content-Type": "application/json" }
+  headers: {
+    'Content-Type': 'application/json',
+    'x-api-key': process.env.REQRES_API_KEY ?? 'reqres-free-v1'
+  }
 };
